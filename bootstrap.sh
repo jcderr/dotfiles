6e8e10f4a57c1e3f5676075eb7b9c058738ba537
Jeremy Derr <jcderr@users.noreply.github.com>
2014-07-29 11:43:09 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,3 +31,4 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/oh-my-zsh ~/.oh-my-zsh
+cd ~/.dotfiles/powerline-fonts && python ./install.py
